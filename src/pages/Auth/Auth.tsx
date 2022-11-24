01160b5af5cfaf8960c414a6edc4481aa74bb3ab
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-24 18:43:06 +0200

    EXPA-01 UPD: logo in page markup added

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 import { AuthBox } from '../../components/Containers/AuthBox';
 import { AuthContainer } from '../../components/Containers/AuthContainer';
 import { PhotoBox } from '../../components/Containers/PhotoBox';
+import { Logo } from '../../components/Logo/Logo';
 import Image from '../../images/auth_back.jpg';
 
 export const Auth: FC = () => {
   return (
     <AuthContainer>
       <AuthBox>
+        <Logo />
         <Outlet />
       </AuthBox>
       <PhotoBox style={{ backgroundImage: `url(${Image})` }}></PhotoBox>{' '}
