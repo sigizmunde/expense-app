01b7d1480b91468234a05e17b159c9e5285ce7a7
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-05 11:34:12 +0200

    EXPA-04 ADD: dashboard header

--- src/pages/Main/Dashboard.tsx
+++ src/pages/Main/Dashboard.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react';
 import { DashboardContainer } from '../../components/DashboardPanels/DashboardContainer';
 import { DashPanel } from '../../components/DashboardPanels/DashPanel';
 import { RightPanel } from '../../components/DashboardPanels/RightPanel';
+import { DashHeader } from '../../components/DashHeader/DashHeader';
 
 export const Dashboard: FC = () => {
   return (
     <DashboardContainer>
-      <DashPanel></DashPanel>
+      <DashPanel>
+        <DashHeader />
+      </DashPanel>
       <RightPanel></RightPanel>
     </DashboardContainer>
   );
