e43b950bdce0dfa6fc323811d60ea8033f55e67c
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-06 13:10:43 +0200

    EXPA-05 UPD: middleware also gets total info

--- src/store/data/dataGetTransactionsMiddleware.ts
+++ src/store/data/dataGetTransactionsMiddleware.ts
@@ -1,12 +1,13 @@
 import { Action, Dispatch, Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
 import { getUser } from '../auth/authThunk';
-import { getTransactions } from './dataThunk';
+import { getTotalInfo, getTransactions } from './dataThunk';
 
 export const dataGetTransactionsMiddleware: Middleware =
   (store: MiddlewareAPI<any>) =>
   (next: Dispatch<Action>) =>
   (action: Action) => {
     if (action.type === getUser.fulfilled.type) {
+      store.dispatch(getTotalInfo());
       store.dispatch(getTransactions({}));
     }
     next(action);
