e7f46b8442ff16a95238880debc67350b570d839
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-07 15:50:14 +0200

    EXPA-04 UPD: table restyled and sorting added

---
+++ src/components/TransactionsTable/TransactionsTable.styled.ts
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
+export const StyledTable = styled(Table)(({ theme }) => ({
+  // minWidth: 700,
+}));
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  border: 0,
+  [`&.${tableCellClasses.head}`]: {
+    ...theme.typography.subtitle2,
+    backgroundColor: theme.palette.primary.contrastText,
+    color: theme.palette.secondary,
+    paddingTop: 0,
+    paddingBottom: theme.spacing(1),
+    whiteSpace: 'nowrap',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    ...theme.typography.subtitle1,
+    color: theme.palette.secondary,
+    minHeight: `calc(${theme.spacing(5)} * 0.8125)`,
+  },
+}));
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(even)': {
+    backgroundColor: theme.palette.custom.violet + '4D',
+  },
+}));
--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,68 +1,52 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import { dataSelectors } from '../../store/data/dataSelectors';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
+
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  StyledTable,
+  StyledTableCell,
+  StyledTableRow,
+} from './TransactionsTable.styled';
 import { moneyNumToString } from '../../utils/moneyNumToString';
 import { theme } from '../../styles/theme';
 import { getTransactions } from '../../store/data/dataThunk';
-import { TableContainer } from '@mui/material';
-
-const StyledTable = styled(Table)(({ theme }) => ({
-  // minWidth: 700,
-}));
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  border: 0,
-  [`&.${tableCellClasses.head}`]: {
-    ...theme.typography.subtitle2,
-    backgroundColor: theme.palette.primary.contrastText,
-    color: theme.palette.secondary,
-    paddingTop: 0,
-    paddingBottom: theme.spacing(1),
-    // borderTop: '2px ' + theme.palette.primary.contrastText + ' solid',
-  },
-  [`&.${tableCellClasses.body}`]: {
-    ...theme.typography.subtitle1,
-    color: theme.palette.secondary,
-    minHeight: `calc(${theme.spacing(5)} * 0.8125)`,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.custom.violet + '4D',
-  },
-}));
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import { TableSortSwitch } from '../TableSortSwitch/TableSortSwitch';
 
 export const TransactionTable: FC = () => {
   const dispatch = useAppDispatch();
-  // dispatch(getTransactions({}));
   const categories = useAppSelector(dataSelectors.getCategories);
   const transactions = useAppSelector(dataSelectors.getTransactions);
+
+  const sort = useAppSelector(dataSelectors.getSort) as {
+    [key: string]: string;
+  }[];
+
+  const valueInSort = (keyToFind: string): string | undefined => {
+    if (sort) {
+      const obj = sort.find((e) => keyToFind in e) as { [key: string]: string };
+      if (!obj) return undefined;
+      return obj[keyToFind];
+    }
+    return undefined;
+  };
+
+  const changeOrder = (keyToChange: string): void => {
+    if (sort) {
+      const newSort = sort.map((e: { [key: string]: string }) => {
+        if (keyToChange in e) {
+          const value =
+            typeof e === 'object' && e[keyToChange] === 'asc' ? 'desc' : 'asc';
+          return { ...e, [keyToChange]: value };
+        }
+        return e;
+      });
+      dispatch(getTransactions({ sort: newSort }));
+    }
+  };
 
   return (
     <StyledTable stickyHeader aria-label="transactions table">
@@ -71,7 +55,15 @@ export const TransactionTable: FC = () => {
           <StyledTableCell>#</StyledTableCell>
           <StyledTableCell>Category</StyledTableCell>
           <StyledTableCell>Name</StyledTableCell>
-          <StyledTableCell>Date</StyledTableCell>
+          <StyledTableCell>
+            Date
+            {sort && valueInSort('date') && (
+              <TableSortSwitch
+                direction={valueInSort('date') === 'asc' ? 'asc' : 'desc'}
+                onClick={() => changeOrder('date')}
+              />
+            )}
+          </StyledTableCell>
           <StyledTableCell align="center">Money</StyledTableCell>
         </TableRow>
       </TableHead>
