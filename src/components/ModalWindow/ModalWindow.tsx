ff9cdf5fe2d1dbae9832f56c60a9a68fd015f4a5
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-09 16:41:23 +0200

    EXPA-04 ADD: modal window component

---
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Modal, ModalProps } from '@mui/material';
+import { Box, styled } from '@mui/system';
+
+const CenteredModal = styled(Modal)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+const ModalContainer = styled(Box)(({ theme }) => ({
+  maxWidth: `calc(${theme.spacing(5)}*8)`,
+  padding: theme.spacing(2),
+  backgroundColor: theme.palette.custom.bgr,
+}));
+
+export const ModalWindow: FC<ModalProps> = ({ children, onClose, open }) => {
+  return (
+    <CenteredModal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <ModalContainer>{children}</ModalContainer>
+    </CenteredModal>
+  );
+};
