34104ed4d8a4a50953ace6d48fa06f501fab3742
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-12 13:31:03 +0200

    EXPA-04 ADD: loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useAppSelector } from '../../hooks/reduxHooks';
+import { authSelectors } from '../../store/auth/authSelectors';
+import { dataSelectors } from '../../store/data/dataSelectors';
+
+export const Loader = () => {
+  const authFetching = useAppSelector(authSelectors.getIsFetching);
+  const dataFetching = useAppSelector(dataSelectors.getIsFetching);
+  const open = authFetching || dataFetching;
+  return (
+    <Backdrop
+      sx={{
+        bgcolor: '#0001',
+        color: '#fff',
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+      }}
+      open={open}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+};
