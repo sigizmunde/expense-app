6637da8390dc0706fe7a6c949aa232ab6548d4d4
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-12-15 12:13:01 +0200

    EXPA-04 UPD: router paths moved to const

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,18 @@ export const LINKS = {
   termsOfUse: 'https://incode-kyiv.embedify.io/',
   policy: 'https://incode-kyiv.embedify.io/corporate-ethics',
 };
+
+export const ROUTES = {
+  AUTH: {
+    index: '/auth',
+    signIn: 'signin',
+    signUp: 'signup',
+  },
+  MAIN: {
+    index: '/',
+    dashboard: 'dashboard',
+    analytics: 'analytics',
+    categories: 'categories',
+    settings: 'settings',
+  },
+};
