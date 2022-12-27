493e2b99970d6dccb24078e6299e9c80a57a3b88
developer <1816579@gmail.com>
2022-12-27 17:44:20 +0200

    EXPA-06 UPD: warning dispatcher

--- src/components/WarningDispatcher/WarningDispatcher.styled.ts
+++ src/components/WarningDispatcher/WarningDispatcher.styled.ts
@@ -1,36 +1,10 @@
-import { Alert, AlertProps } from '@mui/material';
+import { Alert } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { TMessageType } from '../../types/uix';
 
-interface ICustomProps {
-  type?: TMessageType;
-}
-
-export const StyledAlert = styled(Alert)<ICustomProps & AlertProps>(
-  ({ theme, type }) => ({
-    position: 'fixed',
-    top: theme.spacing(5),
-    left: '50%',
-    transform: 'translateX(-50%)',
-    zIndex: 9,
-    backgroundColor: `${() => {
-      switch (type) {
-        case 'err': {
-          return theme.palette.error.main;
-        }
-        case 'warn': {
-          return theme.palette.custom.orange;
-        }
-        case 'info': {
-          return theme.palette.custom.blue70;
-        }
-        case 'success': {
-          return theme.palette.success.main;
-        }
-        default: {
-          return theme.palette.custom.blue70;
-        }
-      }
-    }}`,
-  })
-);
+export const StyledAlert = styled(Alert)(({ theme }) => ({
+  position: 'fixed',
+  top: theme.spacing(5),
+  left: '50%',
+  transform: 'translateX(-50%)',
+  zIndex: 9,
+}));
--- src/components/WarningDispatcher/WarningDispatcher.tsx
+++ src/components/WarningDispatcher/WarningDispatcher.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-
+import Snackbar from '@mui/material/Snackbar';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import { uixSelectors } from '../../store/uix/uixSelectors';
 import { resetMessage } from '../../store/uix/uixSlice';
@@ -7,22 +7,34 @@ import { StyledAlert } from './WarningDispatcher.styled';
 
 export function WarningDispatcher() {
   const dispatch = useAppDispatch();
+  const [open, setOpen] = useState(false);
   const message = useAppSelector(uixSelectors.getMessage);
 
-  const timeoutHandle: ReturnType<typeof setTimeout> = setTimeout(() => {
-    dispatch(resetMessage());
-  }, 2500);
-
   useEffect(() => {
-    return () => {
-      clearTimeout(timeoutHandle);
-      dispatch(resetMessage());
+    setOpen(!!message.text && message.text !== '');
+    // dispatch(resetMessage);
+    return function Cleanup() {
+      dispatch(resetMessage);
     };
-  }, [dispatch, timeoutHandle]);
+  }, [message.text, dispatch]);
 
-  return message.text && message.text !== '' ? (
-    <StyledAlert severity="warning">{message.text}</StyledAlert>
-  ) : (
-    <> </>
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <>
+      {message.text && message.text !== '' && (
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+          <StyledAlert severity={message.type}>{message.text}</StyledAlert>
+        </Snackbar>
+      )}{' '}
+    </>
   );
 }
