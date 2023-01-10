d5ce85bbf6c42d9b719f7f72690b2bd8715438a3
developer <1816579@gmail.com>
2023-01-10 16:51:06 +0200

    EXPA-07 UPD: responsive typography added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,12 +12,12 @@ declare module '@mui/material/styles' {
   }
 }
 
-export const theme = createTheme({
+const initialTheme = createTheme({
   breakpoints: {
     values: {
       xs: 320,
       sm: 600,
-      md: 900,
+      md: 940,
       lg: 1200,
       xl: 1536,
     },
@@ -61,52 +61,86 @@ export const theme = createTheme({
       fontSize: 16,
       color: 'inherit',
     },
+  },
+});
+
+const { breakpoints } = initialTheme;
+
+export const theme = createTheme(initialTheme, {
+  typography: {
     h1: {
       textTransform: 'uppercase',
       fontSize: '56px',
       fontWeight: 700,
       letterSpacing: '0.03',
-      lineHeight: '84px',
+      lineHeight: '1.5',
+      [breakpoints.down('md')]: {
+        fontSize: '45px',
+      },
     },
     h2: {
       fontSize: '48px',
       fontWeight: 700,
-      lineHeight: '72px',
+      lineHeight: '1.5',
+      [breakpoints.down('md')]: {
+        fontSize: '38px',
+      },
     },
     h3: {
       fontSize: '36px',
       fontWeight: 700,
-      lineHeight: '54px',
+      lineHeight: '1.5',
+      [breakpoints.down('md')]: {
+        fontSize: '28px',
+      },
     },
     h4: {
       fontSize: '24px',
       fontWeight: 600,
       lineHeight: '36px',
+      [breakpoints.down('md')]: {
+        fontSize: '18px',
+      },
     },
     h5: {
       fontSize: '16px',
       fontWeight: 600,
       lineHeight: '25px',
+      [breakpoints.down('md')]: {
+        fontSize: '12px',
+      },
     },
     body1: {
       fontSize: '20px',
       fontWeight: 400,
       lineHeight: '31px',
+      [breakpoints.down('md')]: {
+        fontSize: '16px',
+      },
     },
     body2: {
       fontSize: '16px',
       fontWeight: 400,
       lineHeight: '25px',
+      [breakpoints.down('md')]: {
+        fontSize: '13px',
+      },
     },
     subtitle1: {
       fontSize: '14px',
       fontWeight: 400,
       lineHeight: '22px',
+      [breakpoints.down('md')]: {
+        fontSize: '11px',
+      },
     },
     subtitle2: {
       fontSize: '12px',
       fontWeight: 400,
       lineHeight: '19px',
+      [breakpoints.down('md')]: {
+        fontSize: '10px',
+      },
     },
   },
 });
