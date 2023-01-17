d58fed473b208be75357c5a115efbce8580ffc3a
developer <1816579@gmail.com>
2023-01-17 12:37:14 +0200

    EXPA-07 UPD: money num formatting function changed

--- src/utils/moneyNumToString.ts
+++ src/utils/moneyNumToString.ts
@@ -1,13 +1,10 @@
-export const moneyNumToString = ({
+const formatShortNum = ({
   amount,
   currencySign = '$',
-  negative = false,
 }: {
   amount: number;
   currencySign?: string;
-  negative?: boolean;
 }) => {
-  if (amount === 0 && negative) return `-${currencySign} 0.00`;
   const convertedString = (Math.round(amount * 100) / 100).toString();
   const split = convertedString.split('.' || ',');
   const formattedArray = [];
@@ -28,4 +25,40 @@ export const moneyNumToString = ({
     }
   }
   return `${formattedArray.join('')}.${(split[1] || '0').padEnd(2, '0')}`;
+};
+
+const formatLongNum = ({
+  amount,
+  currencySign = '$',
+}: {
+  amount: number;
+  currencySign?: string;
+}) => {
+  const absAmount = Math.abs(amount);
+  const billions = Math.floor(absAmount / 1000000000);
+  const millions = Math.floor((absAmount % 1000000000) / 1000000);
+  const thousands = Math.round((absAmount % 1000000) / 1000);
+  const sign = amount < 0 ? '-' : '';
+  const b = billions ? ` ${billions}B` : '';
+  const m = millions ? ` ${millions + Number(billions > 0)}M` : '';
+  const k = !billions && thousands ? ` ${thousands}k` : '';
+  return sign + currencySign + b + m + k;
+};
+
+export const moneyNumToString = ({
+  amount,
+  currencySign = '$',
+  negative = false,
+}: {
+  amount: number;
+  currencySign?: string;
+  negative?: boolean;
+}) => {
+  if (amount === 0 && negative) return `-${currencySign} 0.00`;
+  switch (true) {
+    case Math.abs(amount) > 9999:
+      return formatLongNum({ amount, currencySign });
+    default:
+      return formatShortNum({ amount, currencySign });
+  }
 };
